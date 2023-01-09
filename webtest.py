2c0e27e710fc2ce5fc840771c34e7611e3027b09
FuS1337 <50884083+FuS1337@users.noreply.github.com>
2023-01-09 13:54:32 +0100

    Update webtest.py

--- webtest.py
+++ webtest.py
@@ -5,14 +5,16 @@ except:
 
 from machine import Pin
 import network
-
+import time
 import gc
+import binascii
+
 gc.collect()
-
-def web_page():
-    html = """<!doctype html><html><head> <title>ESP Web Server</title> <meta name="viewport"></head><body> <h1>ESP Web Server</h1></body></html>"""
+ 
+def web_page(text):
+    html = """<!doctype html><html><head> <title>ESP Web Server</title> <meta name="viewport/"></head><body> <h1>ESP Web Server</h1><a>Hauptfarbe des Bildes (Rot, Gruen, Blau, weder noch): """ + text + """</a></body></html>"""
     return html
-
+ 
 def web(text):
    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
    s.bind(('', 80))
@@ -22,10 +24,10 @@ def web(text):
      print('Got a connection from %s' % str(addr))
      request = conn.recv(1024)
      request = str(request)
-     pritn("-----------------------------------------------------")
+     print("-----------------------------------------------------")
      print('Content = %s' % request)
-     pritn("-----------------------------------------------------")
-     response = web_page()
+     print("-----------------------------------------------------")
+     response = web_page(text)
      conn.send('HTTP/1.1 200 OK\n')
      conn.send('Content-Type: text/html\n')
      conn.send('Connection: close\n\n')
