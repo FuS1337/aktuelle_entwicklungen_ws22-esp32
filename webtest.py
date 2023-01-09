b5adc12a0de0f0f8693c49b0f4d8c67aa30727b6
FuS1337 <50884083+FuS1337@users.noreply.github.com>
2023-01-09 09:38:28 +0100

    Update webtest.py
    web server fixed

--- webtest.py
+++ webtest.py
@@ -8,15 +8,26 @@ import network
 
 import gc
 gc.collect()
- 
+
+def web_page():
+    html = """<!doctype html><html><head> <title>ESP Web Server</title> <meta name="viewport"></head><body> <h1>ESP Web Server</h1></body></html>"""
+    return html
+
 def web(text):
-   html = """<!doctype html><html><head> <title>ESP Web Server</title> <meta name="viewport"></head><body> <h1>ESP Web Server</h1></body></html>"""
    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
    s.bind(('', 80))
    s.listen(5)
    while True:
      conn, addr = s.accept()
      print('Got a connection from %s' % str(addr))
-     conn.send('HTTP/1.0 200 OK\r\nContent-type: text/html\r\n\r\n')
-     conn.send(html)
+     request = conn.recv(1024)
+     request = str(request)
+     pritn("-----------------------------------------------------")
+     print('Content = %s' % request)
+     pritn("-----------------------------------------------------")
+     response = web_page()
+     conn.send('HTTP/1.1 200 OK\n')
+     conn.send('Content-Type: text/html\n')
+     conn.send('Connection: close\n\n')
+     conn.sendall(response)
      conn.close()
