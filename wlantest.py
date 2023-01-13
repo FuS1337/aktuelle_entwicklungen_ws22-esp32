1c1804c0b4ce1f858677cbd529839c0b7e55df1a
FuS1337 <50884083+FuS1337@users.noreply.github.com>
2023-01-13 11:33:26 +0100

    Update wlantest.py

--- wlantest.py
+++ wlantest.py
@@ -2,8 +2,6 @@ try:
   import usocket as socket
 except:
   import socket
-
-from machine import Pin
 import network
 
 import esp
@@ -13,17 +11,11 @@ import gc
 gc.collect()
 
 def connwlan(ssid, password):
-    #ssid = 'GWH.TEL-SM1T6X8K'
     station = network.WLAN(network.STA_IF)
-    #print(station.scan())
-
     station.active(True)
     station.connect(ssid, password)
-
     while station.isconnected() == False:
       pass
-
     print('Connection successful')
     print(station.ifconfig())
 
-    led = Pin(2, Pin.OUT)
